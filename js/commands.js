a209689de3a87b0bd99417e70e933d9d693df901
Eboxsl <144025781+Eboxsl@users.noreply.github.com>
2024-06-28 18:30:50 +0530

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -78,8 +78,8 @@ banner = [
   "| (____/\    | )___) )| (___) |( /   \ )",
   "(_______/    |/ \___/ (_______)|/     \|",
   "",
-  "",
-  "",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
   '<span class="color2">Welcome to my interactive web terminal.</span>',
   "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
 ];
