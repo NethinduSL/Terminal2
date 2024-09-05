945cc461f2d031c0313b759aad535d1667041ac5
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-09-05 18:07:43 +0530

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,85 +1,216 @@
 var youtube = "https://www.youtube.com/Mechstrikesl/";
+
 var twitter = "https://www.twitter.com/";
-var password = "fkcodes";
+
+var password = "EBOX@n2009";
+
 var linkedin = "https://www.linkedin.com/in/";
+
 var instagram = "https://www.instagram.com/ebox@nt/";
+
 var github = "https://github.com/bit-x-team/";
-var email = 'mailto:forrest@fkcodes.com';
+
+var email = 'mailto:Nethindu@ebox.free.nf';
+
+
 
 whois = [
+
   "<br>",
-  "Hey, I'm Nethindu jithula podi eko😂!👋",
+
+  "Hey, I'm Nethindu 😂👋🙂",
+
   "I'm a software engineer and content creator, who builds engaging websites like this one",
+
   "and makes YouTube videos about computer science & software engineering.",
+
   "After graduating with a Bachelor's in Computer Science, I worked professionally",
+
   "as a software engineer building enterprise web applications for Fortune 500 companies.",
+
   "While doing all of that, I documented my coding journey on YouTube - trying to enlighten",
+
   "the next generation of developers and help them navigate the crazy world that is software", "engineering & computer science.",
+
   "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+
   "I needed to make the jump from software engineering to full time content creation - strike while",
+
   "the iron's hot.",
+
   "Now, I make videos about software engineering and computer science, building anything my heart desires,",
+
   "like this terminal website.",
+
   "<br>"
+
 ];
+
+
 
 whoami = [
+
   "<br>",
+
   "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+
   "<br>"
+
 ];
+
+
 
 social = [
+
   "<br>",
-  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
-  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
-  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
-  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
-  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/mechstrike' + "</a>",
+
+  'twitter        <a href="' + twitter + '" target="_blank">twitter' + '</a>',
+
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin' + "</a>",
+
+  'instagram      <a href="' + instagram + '" target="_blank">instagram' + '</a>',
+
+  'github         <a href="' + github + '" target="_blank">github' + "</a>",
+
   "<br>"
+
 ];
 
-secret = [
+
+
+pass = [
+
   "<br>",
-  '<span class="command">sudo</span>           Only use if you\'re admin',
-  "<br>"
+  'ඔයා Nethindu  නෙමෙයි නම් ඔයාට',' කෙලවෙලාම යන්න ඕන 🤬 ','Devise එක ගිනි ගන්න ඕන🔥',
+    
+  '<span class="command">Emails</span>                               Password','',
+  '<span class="command">Eboxsrilanka2009@gmail.com </span>          EB**@0763******',
+  '<span class="command">ict10mcg@gmail.com </span>                  I**10@mc*2**4',
+  '<span class="command">Mechstrikesl@gmail.com</span>               M***S*****@sn',
+  '<span class="command">Bitxsrilanka@gmail.com </span>              B***x@NJ**23',
+
+    "<br>"
+
 ];
+
+
 
 projects = [
+
   "<br>",
+
   "Still curating... most projects are offline, on GitHub, or confidential.",
+
   "<br>"
+
 ];
 
-help = [
+
+
+menu = [
+
   "<br>",
-  '<span class="command">whois</span>          Who is Nethindu ?',
-  '<span class="command">whoami</span>         Who are you?',
-  '<span class="command">video</span>          View YouTube videos',
+
+  '<span class="command">whois</span>          Who is Nethindu මම කව්ද?',
+
+  '<span class="command">whoami</span>         Who are you? ඔයා කව්ද?',
+
+  '<span class="command">video</span>          View YouTube videos mech arena',
+
   '<span class="command">social</span>         Display social networks',
-  '<span class="command">secret</span>         Find the password',
+
+  '<span class="command">secret</span>         Find the password පුලුවන්නම් හොයන්න😈',
+
   '<span class="command">projects</span>       View coding projects',
+
   '<span class="command">history</span>        View command history',
-  '<span class="command">help</span>           You obviously already know what this does',
+
+  '<span class="command">help</span>           You obviously already know what this does මේක කොහොමත් දන්නෝ දැන්',
+
   '<span class="command">email</span>          Do not email me',
+
   '<span class="command">clear</span>          Clear terminal',
+  
   '<span class="command">banner</span>         Display the header',
+
   "<br>",
+
 ];
+
+
 
 banner = [
+
   '<span class="index">E Box Nethindu . All knights reserved.</span>',
-  "_______      ______   _______          ",
-  "|(  ____ \    (  ___ \ (  ___  )|\     /   ",
-  "| (    \/    | (   ) )| (   ) |( \   / ) ",
-  "| (__        | (__/ / | |   | | \ (_) / ",
-  "|  __)       |  __ (  | |   | |  ) _ (  ",
-  "| (          | (  \ \ | |   | | / ( ) \ ",
-  "| (____/\    | )___) )| (___) |( /   \ ) ",
-  "(_______/    |/ \___/ (_______)|/     \|",
+
+  "_______      ______      _______          ",
+  "| (____\\      (  ___ \\  (  ___  ) |\\     /   ",
+  "| (   \\/     | (   ) )  | (   ) | ( \   / ) ",
+  "| (__        | (__/ /   | |   | |  \\ (_) / ",
+  "|  __)       |  __ (    | |   | |   ) _ (  ",
+  "| (          | (  \\ \\   | |   | |  / ( ) \\ ",
+  "| (____/\\    | )___) )  | (___) | ( /   \\ ) ",
+  "(_______/    |/ \\___/   (_______) |/     \\|",
+
   "                                  © 2022",
+
   "     ",
+
   "",
+
   '<span class="color2">Welcome to Nethindu😈s interactive web terminal.</span>',
-  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'alive'</span><span class=\"color2\">.</span>",
+
 ];
+
+alive = [
+
+  "<br>",
+
+  '╭───────────────╮',
+  '    𝗘 𝗧𝗘𝗥𝗠𝗜𝗡𝗔𝗟𝗔 ',
+  '╰───────────────╯',
+  '',
+  '╭──────────────────',
+  '│Hello there I am Online !  ࿐',
+  '│*🧑‍💻 Devoloper  :*Nethindu ',
+  '',
+  '│*🧬 Version :* 2.0',
+   '',
+  '│*🗃️ Public Group :* https:',
+  '╰──────────────────',
+  '',
+  '😌⃟⌘Nethindu ❤️✾ـٰٰٰٰٖٖٖٖٜ۬ـٰٰٰٖٖٖٜ۬ـٰٰٖٖٜ۬ـٰٖٜ۬ـٰٖٜ۬ـٰٰٖٖٜ۬ـٰٰٰٖٖٖٜ۬ـٰٰٰٰٖٖٖٖٜ۬ـٰٰٰٖٖٖٜ۬ـٰٰٖٖٜ۬ـٰٖٜ۬ـٰٖٜ۬ـٰٰٖٖٜ۬ـٰٰٰٖٖٖٜ۬ـٰٰٰٰٖٖٖٖٜ۬✾➣',
+  '',
+  '  • - - - - - - ☆- - - - - - •',
+  '',
+  ' ♡ ㅤ      ❍ㅤ        ⎙ㅤ    ⌲',
+  ' Hart     love     save     share',
+  ' ●━━━━━━━━━●━━━━━●',
+  '   ⇆   ◁ㅤㅤ❚❚ㅤㅤ▷      ↻',
+  '                                  ✯',
+  '',
+  '  By Nethindu 😈',
+
+ "<br>",
+
+];
+
+nt = [
+
+  "<br>",
+
+  '╭──────────────────╮',
+  ' I love you developer💝',
+  '╰──────────────────╯',
+  '',
+];
+
+contact = [
+    'Whatsapp  ✆       <a href="https:/ /wa.me/94766428832?text=Hi_EBox" target="_blank">Nethindu click me' + "</a>",
+];
+
+
